bf6080707320884de797926de3ab69353608b42d
Prasad <Prasad@DNV>
2021-01-17 19:56:31 +0530

    My First Commit

---
+++ DemoProj222/src/amazonPack/Class1.java
@@ -0,0 +1,10 @@
+package amazonPack;
+
+public class Class1 {
+	
+	public void login()
+	{
+		System.out.println("Login Page");
+	}
+
+}
---
+++ DemoProj222/src/amazonPack/Class2.java
@@ -0,0 +1,10 @@
+package amazonPack;
+
+public class Class2 {
+	
+	public void homePage()
+	{
+		System.out.println("HomePage");
+	}
+
+}
