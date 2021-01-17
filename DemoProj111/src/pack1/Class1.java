4da960035ef18e627612fd35e6fdd937ced219c0
Prasad <Prasad@DNV>
2021-01-17 19:40:19 +0530

    First Commit

---
+++ DemoProj111/src/pack1/Class1.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Class1 {
+	
+	public void m1()
+	{
+		System.out.println("m1");
+	}
+
+}
---
+++ DemoProj111/src/pack1/Class2.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class Class2 {
+	
+	public void m2()
+	{
+		System.out.println("m2");
+	}
+
+}
